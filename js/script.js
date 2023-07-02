2d9139d5ae67a884983faf7a49d0a3c61e36cacd
Gl29 <glebkovganich@gmail.com>
2023-07-02 19:55:05 +0300

    update_1

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
   {arrows:true,
     dots:true,
     adaptiveHeight:true,
-    variableWidth: true,
+
+    variableWidth: false,
 
     centerMode: true,
 
@@ -16,8 +17,35 @@ $(document).ready(function(){
 
     waitForAnimate:false,
 
-    mobileFirst: true
+    mobileFirst: true,
   
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+
+
   }
  );
 
