f7c183f4be729449341edb99458c3e395fcd6cfd
Gl29 <glebkovganich@gmail.com>
2023-07-02 17:58:45 +0300

    first version

---
+++ js/script.js
@@ -0,0 +1,59 @@
+// jquery
+$(document).ready(function(){
+ $('.slider_1').slick(
+  {arrows:true,
+    dots:true,
+    adaptiveHeight:true,
+    variableWidth: true,
+
+    centerMode: true,
+
+    slidesToShow: 2,
+    slidesToScroll:2,
+    speed: 800,
+    // autoplay: true,
+    autoplaySpeed:3000,
+
+    waitForAnimate:false,
+
+    mobileFirst: true
+  
+  }
+ );
+
+
+
+});
+
+
+
+// function outputElementHeight() {
+//   var element = document.querySelector('.listOfWorks_interior');
+//   var height = element.offsetHeight;
+//   console.log('Висота елемента listOfWorks_interior: ' + height + 'px');
+
+
+
+//   var sliderItem = document.querySelector('.slider_item');
+
+
+//   if (sliderItem) {
+//     var imgElement = sliderItem.querySelector('img');
+//     var tHeight = height+ 'px'
+//     console.log('Висота елемента imgElement до зміни = : ' + imgElement.height + 'px');
+//     console.log('tHeight = : ' + tHeight);
+
+//     sliderItem.style.maxHeight=  tHeight; // Задайте нове значення max тут
+//     imgElement.height =  tHeight;
+//     console.log('Висота елемента imgElement після зміни = : ' + imgElement.height + 'px');
+//   }
+
+
+ 
+  
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   outputElementHeight();
+//   window.addEventListener('resize', outputElementHeight);
+// });
